9c10d6a444b79cff67fd1c85d83eb208730368bd
Caio <caiob1305@gmail.com>
2023-10-11 01:09:52 -0300

    feat: user repository

--- back/src/modules/users/repositories/in-memory/user.in-memory.repository.ts
+++ back/src/modules/users/repositories/in-memory/user.in-memory.repository.ts
@@ -2,6 +2,7 @@ import { plainToInstance } from 'class-transformer';
 import { CreateUserDto } from '../../dto/create-user.dto';
 import { User } from '../../entities/user.entity';
 import { UsersRepository } from '../user.repository';
+import { UpdateUserDto } from '../../dto/update-user.dto';
 
 export class UsersInMemoryRepository implements UsersRepository {
   private database: User[] = [];
@@ -13,7 +14,28 @@ export class UsersInMemoryRepository implements UsersRepository {
     this.database.push(newUser);
     return plainToInstance(User, newUser);
   }
+
   async findAll(): Promise<User[]> {
     return plainToInstance(User, this.database);
+  }
+
+  async findById(id: string): Promise<User> {
+    const user = this.database.find((user) => user.id === id);
+    return plainToInstance(User, user);
+  }
+
+  async update(data: UpdateUserDto, id: string): Promise<User> {
+    const userIndex = this.database.findIndex((user) => user.id === id);
+    this.database[userIndex] = {
+      ...this.database[userIndex],
+      ...data,
+    };
+
+    return plainToInstance(User, this.database[userIndex]);
+  }
+
+  async remove(id: string): Promise<void> {
+    const userIndex = this.database.findIndex((user) => user.id === id);
+    this.database.splice(userIndex, 1);
   }
 }
--- back/src/modules/users/repositories/user.repository.ts
+++ back/src/modules/users/repositories/user.repository.ts
@@ -1,10 +1,11 @@
 import { CreateUserDto } from '../dto/create-user.dto';
+import { UpdateUserDto } from '../dto/update-user.dto';
 import { User } from '../entities/user.entity';
 
 export abstract class UsersRepository {
   abstract create(data: CreateUserDto): Promise<User>;
   abstract findAll(): Promise<User[]>;
   abstract findById(id: string): Promise<User | null>;
-  // abstract update(data: UpdateUserDto, id: string): Promise<User>;
+  abstract update(data: UpdateUserDto, id: string): Promise<User>;
   abstract remove(id: string): Promise<void>;
 }
