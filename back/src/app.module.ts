d4d330b59d0beebe8cada20a2d23ba2f58256c5a
Caio <caiob1305@gmail.com>
2023-10-16 10:47:48 -0300

    Feat: Address Create

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { UsersModule } from './modules/users/users.module';
 import { AnouncementsModule } from './modules/anouncements/anouncements.module';
 import { AuthModule } from './modules/auth/auth.module';
+import { AddressModule } from './modules/address/address.module';
+import { AddressModule } from './modules/address/address.module';
 
 @Module({
-  imports: [UsersModule, AnouncementsModule, AuthModule],
+  imports: [UsersModule, AnouncementsModule, AuthModule, AddressModule],
   controllers: [],
   providers: [],
 })
