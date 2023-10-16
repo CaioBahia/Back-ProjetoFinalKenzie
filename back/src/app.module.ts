2b101c5972d762683feaeb1a8c8d01ac68b3eaa9
Caio <caiob1305@gmail.com>
2023-10-16 05:43:35 -0300

    Feat: Auth Logic

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './modules/users/users.module';
 import { AnouncementsModule } from './modules/anouncements/anouncements.module';
+import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
-  imports: [UsersModule, AnouncementsModule],
+  imports: [UsersModule, AnouncementsModule, AuthModule],
   controllers: [],
   providers: [],
 })
