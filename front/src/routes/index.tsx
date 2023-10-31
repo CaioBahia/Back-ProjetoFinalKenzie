e0c1395f0e87c279fd6a6c8835e37959fcc3d004
Caio <caiob1305@gmail.com>
2023-10-31 01:09:11 -0300

    feat: page news

--- front/src/routes/index.tsx
+++ front/src/routes/index.tsx
@@ -2,6 +2,10 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import { LoginPage } from "../pages/LoginPage/login";
 import { HomePage } from "../pages/HomePage";
+import { RegisterPage } from "../pages/RegisterPage";
+import { ProductPage } from "../pages/ProductPage";
+import { ProfilePage } from "../pages/ProfilePage";
+import { NotFoundPage } from "../pages/NotFound";
 
 export const RoutesMain = () => {
   return (
@@ -11,13 +15,13 @@ export const RoutesMain = () => {
       {/* login */}
       <Route path="/login" element={<LoginPage />} />
       {/*  Register */}
-      <Route path="" element={} />
+      <Route path="" element={<RegisterPage />} />
       {/* Product */}
-      <Route path="" element={} />
+      <Route path="" element={<ProductPage />} />
       {/* Profile */}
-      <Route path="" element={} />
+      <Route path="" element={<ProfilePage />} />
       {/* 404 */}
-      <Route path="*" element={} />
+      <Route path="*" element={<NotFoundPage />} />
     </Routes>
   );
 };
